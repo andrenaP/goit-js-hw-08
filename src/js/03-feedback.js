4c65018034427138f964c5ea1658a75cdc35279d
andrenaP <a0993100106@gmail.com>
2023-05-30 12:41:34 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,13 @@
 import throttle from 'lodash.throttle';
 const form_1 = document.querySelector('.feedback-form');
-const KEY = 'form';
+const KEY = 'feedback-form-state';
 startForm();
 form_data = {};
 function startForm() {
     let Form_save = localStorage.getItem(KEY);
     if (Form_save) {
-        Form_save = JSON.parse(Form_save);
-        Object.entries(Form_save).forEach(([name, value]) => {
+        form_data = JSON.parse(Form_save);
+        Object.entries(form_data).forEach(([name, value]) => {
             form_1.elements[name].value = value;
         });
     }
@@ -15,15 +15,22 @@ function startForm() {
 
 form_1.addEventListener('submit', onFormSubmit);
 form_1.addEventListener('input', throttle(onFormInput, 500));
+
 function onFormSubmit(event) {
     event.preventDefault();
-    form_data[event.target.name] = event.target.value
-    console.log(form_data);
+    const formData = new FormData(form_1);
+    formData.forEach((value, name) => console.log(value, name));
     event.currentTarget.reset();
     localStorage.removeItem(KEY);
 }
 function onFormInput(event) {
-    form_data[event.target.name] = event.target.value
-    localStorage.setItem(KEY, JSON.stringify(form_data));
+    let form_data = localStorage.getItem(KEY);
 
+    form_data = JSON.parse(form_data);
+    if (form_data)
+        form_data[event.target.name] = event.target.value
+    else
+        form_data = {};
+
+    localStorage.setItem(KEY, JSON.stringify(form_data));
 }
